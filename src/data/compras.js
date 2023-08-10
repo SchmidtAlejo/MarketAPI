f31dffe53a9b72271a163444e4ce98ac544c11be
SchmidtAlejo <schmidtalejo@gmail.com>
2023-08-10 20:06:11 -0300

    getCompra joins

--- src/data/compras.js
+++ src/data/compras.js
@@ -1,15 +1,27 @@
 const modeloCompras = require("./../../models").Compras;
+const modeloCarritos = require("./../../models").Carritos;
+const modeloCarritosItems = require("./../../models").CarritosItems;
+const modeloProductos = require("./../../models").Productos;
 
-async function addCompra(body){
+async function addCompra(body) {
     return await modeloCompras.create(body);
 }
 
-async function getCompraByPk(id){
-    return await modeloCompras.findByPk(id);
+async function getCompraByPk(id) {
+    return await modeloCompras.findOne({
+        where: {id: id},
+        include: [{ model: modeloCarritos, include: [{model: modeloCarritosItems, include: modeloProductos}] }],
+    });
 }
 
-async function getCompras(usuarioId){
-    return {compras: await modeloCompras.findAll({where: {usuarioId: usuarioId}})};
+async function getCompras(usuarioId) {
+    return {
+        compras: await modeloCompras.findAll({
+            where: { usuarioId: usuarioId },
+            include: [{ model: modeloCarritos, include: modeloCarritosItems }],
+            order: [["createdAt", 'DESC']],
+        })
+    };
 }
 
-module.exports= {addCompra, getCompraByPk, getCompras};
+module.exports = { addCompra, getCompraByPk, getCompras };
