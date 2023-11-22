cb64353bf95724c19a6ddf1fc233ed66e0acf5d1
SchmidtAlejo <schmidtalejo@gmail.com>
2023-11-22 17:11:18 -0300

    remove pool

--- config/config.js
+++ config/config.js
@@ -37,18 +37,6 @@ module.exports = {
       ssl: {
         require: true,
       }
-    },
-    pool: {
-      min: 0,
-      max: 7,
-      acquireTimeoutMillis: 300000,
-      createTimeoutMillis: 300000,
-      destroyTimeoutMillis: 50000,
-      idleTimeoutMillis: 300000,
-      reapIntervalMillis: 10000,
-      createRetryIntervalMillis: 2000,
-      propagateCreateError: false,
-    },
-    acquireConnectionTimeout: 60000,
+    }
   }
 };
